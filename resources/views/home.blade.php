c3720bf49c04a8bd61412647574b2e764958cade
RobertoVillani <robertovillani58@gmail.com>
2022-02-04 18:05:41 +0100

    card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,25 @@
-<ul>
-    @foreach ($movies as $movie)
-        <li>{{$movie->original_title}}</li>
-    @endforeach
-</ul>
+@extends('layouts.base')
+
+@section('pageTitle')
+    Home
+@endsection
+
+@section('pageContent')
+<div class="home">
+    <div class="container">
+        <div class="row">
+
+            @foreach ($movies as $movie)
+                <div class="card">
+                    <h2>{{$movie['title']}}</h2>
+                    <h3>Titolo originale: {{$movie['original_title']}}</h3>
+                    <span><strong>Nazionalità:</strong> {{$movie['nationality']}}</span>
+                    <span><strong>Data di uscita:</strong> {{$movie['date']}}</span>
+                    <span><strong>Voto:</strong> {{$movie['vote']}}</span>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+</div>
+@endsection
